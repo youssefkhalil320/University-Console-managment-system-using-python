336215cbd18e46ed80f5dc2ee528c130e0750088
Noran Mohamed <103522611+noran-mohamed@users.noreply.github.com>
2024-07-07 23:38:08 +0900

    Error Handling StudentManagemnt.py
    global OurStudents was important at the first of each function so that the changes made by these functions affect the OurStudents list.

--- modules/StudentManagemnt.py
+++ modules/StudentManagemnt.py
@@ -4,21 +4,26 @@ from Worker import  Worker
 OurStudents = []
 
 def add_student(student):
+    global OurStudents
     OurStudents.append(student)
 
 def find_student_by_name(name):
+    global OurStudents
     for student in OurStudents:
         if student.getName() == name:
             return student.description()
 
 def find_student_by_id(sid):
+    global OurStudents
     for student in OurStudents:
         if student.getId() == sid:
             return student.description()       
 
 
 def sort_student_by_name():
+    global OurStudents
     OurStudents = sorted(OurStudents, key = lambda x:x.name) 
 
 def sort_student_by_id():
+    global OurStudents
     OurStudents = sorted(OurStudents, key = lambda x:x.id)        
